23d5ddd3e566bc7a989f0942a8427b37359bd8db
frankknoll <Knoll_Frank@web.de>
2026-06-19 15:56:49 +0200

    removing a TODO

--- app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/GoogleMapsNavigator.java
+++ app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/GoogleMapsNavigator.java
@@ -29,7 +29,6 @@ public class GoogleMapsNavigator {
 				.append(",")
 				.append(origin.lng());
 
-		// FK-TODO: der letzte optimizedStop muß nicht unbedingt das Ziel sein. Wie ist das Destination vs. Waypoint in der Original-Teilen-URL von Google Maps mit dem data-Parameter hinterlegt? Unit-Test dazu schreiben.
 		// 3. Set the exact coordinates for the final destination
 		final RouteOptimizer.Stop destination = optimizedStops.get(optimizedStops.size() - 1);
 		urlBuilder
