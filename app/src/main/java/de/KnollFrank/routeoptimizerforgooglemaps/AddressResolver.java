5fd527e5e0256943ce3cf68e674c2c141bde9888
frankknoll <Knoll_Frank@web.de>
2026-06-20 15:32:11 +0200

    adding a TODO

--- app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/AddressResolver.java
+++ app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/AddressResolver.java
@@ -11,6 +11,7 @@ import java.util.Locale;
 
 public class AddressResolver {
 
+	// FK-TODO: Geocoder soll nur eine Möglichkeit von mehreren sein, darunter https://github.com/komoot/photon
 	private final Geocoder geocoder;
 
 	public AddressResolver(final Context context) {
