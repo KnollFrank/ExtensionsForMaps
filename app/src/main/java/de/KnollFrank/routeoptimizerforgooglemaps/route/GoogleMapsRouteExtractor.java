c27a8af03133fda841fc6f052634cbd52fcd1855
frankknoll <Knoll_Frank@web.de>
2026-07-09 10:37:27 +0200

    adding a TODO

--- app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/route/GoogleMapsRouteExtractor.java
+++ app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/route/GoogleMapsRouteExtractor.java
@@ -28,6 +28,7 @@ public class GoogleMapsRouteExtractor {
                 .ifPresent(
                         waypointContainers -> {
                             // 3. Extrahiere die Daten für jeden Stopp aus seinem jeweiligen Sub-Baum
+                            // FK-TODO: use Lists.zip(stopDataList, waypointContainers);
                             for (int i = 0; i < waypointContainers.size(); i++) {
                                 final Node waypointNode = waypointContainers.get(i);
                                 final StopData stopData = stopDataList.get(i);
