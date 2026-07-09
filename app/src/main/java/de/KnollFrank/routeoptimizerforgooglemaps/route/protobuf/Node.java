9cc18bf2c622d93a2f07d7997415af5abd6b9cf7
frankknoll <Knoll_Frank@web.de>
2026-07-09 11:18:21 +0200

    refactoring

--- app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/route/protobuf/Node.java
+++ app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/route/protobuf/Node.java
@@ -10,7 +10,6 @@ import java.util.StringJoiner;
 // FK-TODO: refactor
 public class Node {
 
-    public final String token;
     // FK-TODO: fieldId und type in einer Markerklasse zusammenfassen, und hier als Optional<Marker> verwenden?
     public final int fieldId;
     // FK-TODO: use Datatype for type
@@ -19,10 +18,13 @@ public class Node {
     public final List<Node> children = new ArrayList<>();
 
     public Node(final int fieldId, final char dataType, String value) {
-        this.token = String.valueOf(fieldId) + String.valueOf(dataType) + value;
         this.fieldId = fieldId;
         this.dataType = dataType;
         this.value = value;
+    }
+
+    public String getToken() {
+        return String.valueOf(fieldId) + String.valueOf(dataType) + value;
     }
 
     public boolean isContainer() {
@@ -30,18 +32,7 @@ public class Node {
     }
 
     public int getContainerSize() {
-        int typeIdx = 0;
-        while (typeIdx < token.length() && Character.isDigit(token.charAt(typeIdx))) {
-            typeIdx++;
-        }
-        if (typeIdx + 1 <= token.length()) {
-            try {
-                return Integer.parseInt(token.substring(typeIdx + 1));
-            } catch (final NumberFormatException e) {
-                return 0;
-            }
-        }
-        return 0;
+        return Integer.parseInt(value);
     }
 
     @Override
@@ -60,10 +51,9 @@ public class Node {
     @Override
     public String toString() {
         return new StringJoiner(", ", Node.class.getSimpleName() + "[", "]")
-                .add("token='" + token + "'")
                 .add("fieldId=" + fieldId)
                 .add("dataType=" + dataType)
-                .add("children=" + children)
+                .add("value='" + value + "'")
                 .toString();
     }
 }
--- app/src/test/java/de/KnollFrank/routeoptimizerforgooglemaps/route/protobuf/NodesParserTest.java
+++ app/src/test/java/de/KnollFrank/routeoptimizerforgooglemaps/route/protobuf/NodesParserTest.java
@@ -18,10 +18,10 @@ public class NodesParserTest {
 
         // Then
         assertEquals(2, nodes.size());
-        assertEquals("1sabc", nodes.get(0).token);
+        assertEquals("1sabc", nodes.get(0).getToken());
         assertEquals(1, nodes.get(0).fieldId);
         assertEquals('s', nodes.get(0).dataType);
-        assertEquals("2d1.23", nodes.get(1).token);
+        assertEquals("2d1.23", nodes.get(1).getToken());
         assertEquals(2, nodes.get(1).fieldId);
         assertEquals('d', nodes.get(1).dataType);
     }
@@ -37,12 +37,12 @@ public class NodesParserTest {
         // Then
         assertEquals(1, nodes.size());
         final Node container = nodes.get(0);
-        assertEquals("1m2", container.token);
+        assertEquals("1m2", container.getToken());
         assertEquals(1, container.fieldId);
         assertEquals('m', container.dataType);
         assertEquals(2, container.children.size());
-        assertEquals("2sabc", container.children.get(0).token);
-        assertEquals("3d4.56", container.children.get(1).token);
+        assertEquals("2sabc", container.children.get(0).getToken());
+        assertEquals("3d4.56", container.children.get(1).getToken());
     }
 
     @Test
@@ -57,10 +57,10 @@ public class NodesParserTest {
         final Node outer = nodes.get(0);
         assertEquals(1, outer.children.size());
         final Node inner = outer.children.get(0);
-        assertEquals("2m2", inner.token);
+        assertEquals("2m2", inner.getToken());
         assertEquals(2, inner.children.size());
-        assertEquals("3sabc", inner.children.get(0).token);
-        assertEquals("4d1.2", inner.children.get(1).token);
+        assertEquals("3sabc", inner.children.get(0).getToken());
+        assertEquals("4d1.2", inner.children.get(1).getToken());
     }
 
     @Test
@@ -76,26 +76,26 @@ public class NodesParserTest {
         // Then
         assertEquals(1, nodes.size());
         final Node root = nodes.get(0);
-        assertEquals("4m22", root.token);
+        assertEquals("4m22", root.getToken());
         assertEquals(1, root.children.size());
 
         final Node child4m21 = root.children.get(0);
-        assertEquals("4m21", child4m21.token);
+        assertEquals("4m21", child4m21.getToken());
         assertEquals(1, child4m21.children.size());
 
         final Node child1m5 = child4m21.children.get(0);
-        assertEquals("1m5", child1m5.token);
+        assertEquals("1m5", child1m5.getToken());
         assertEquals(1, child1m5.children.size());
 
         final Node child1m4 = child1m5.children.get(0);
-        assertEquals("1m4", child1m4.token);
+        assertEquals("1m4", child1m4.getToken());
         assertEquals(2, child1m4.children.size());
 
-        assertEquals("1s0x4799fc4b13515dd5:0x345201aaff119b3a", child1m4.children.get(0).token);
+        assertEquals("1s0x4799fc4b13515dd5:0x345201aaff119b3a", child1m4.children.get(0).getToken());
         final Node child8m2 = child1m4.children.get(1);
-        assertEquals("8m2", child8m2.token);
+        assertEquals("8m2", child8m2.getToken());
         assertEquals(2, child8m2.children.size());
-        assertEquals("3d48.4765345", child8m2.children.get(0).token);
-        assertEquals("4d8.934900899999999", child8m2.children.get(1).token);
+        assertEquals("3d48.4765345", child8m2.children.get(0).getToken());
+        assertEquals("4d8.934900899999999", child8m2.children.get(1).getToken());
     }
 }
