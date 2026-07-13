d962a2ef82a60ef2c17ba9c943b20c672fa7feab
frankknoll <Knoll_Frank@web.de>
2026-07-13 09:40:18 +0200

    refactoring

--- app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/route/protobuf/NodeParser.java
+++ app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/route/protobuf/NodeParser.java
@@ -1,25 +1,49 @@
 package de.KnollFrank.routeoptimizerforgooglemaps.route.protobuf;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class NodeParser {
 
-    // FK-TODO: refactor
-    // token = [fieldId][dataType][value]
+    private static final String FIELD_ID = "fieldId";
+    private static final String DATA_TYPE = "dataType";
+    private static final String VALUE = "value";
+    private static final Pattern TOKEN_PATTERN =
+            Pattern.compile(
+                    String.format(
+                            "^(?<%s>\\d+)(?<%s>.)(?<%s>.*)$",
+                            FIELD_ID, DATA_TYPE, VALUE));
+
     public static Node parseNode(final String token) {
-        final String fieldId = getFieldId(token);
+        final Matcher matcher = TOKEN_PATTERN.matcher(token);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(token);
+        }
         return new Node(
-                Integer.parseInt(fieldId),
-                new Datatype(token.charAt(fieldId.length())),
-                token.substring(fieldId.length() + 1),
+                getFieldId(matcher),
+                getDatatype(matcher),
+                getValue(matcher),
                 List.of());
     }
 
-    private static String getFieldId(final String token) {
-        int index = 0;
-        while (index < token.length() && Character.isDigit(token.charAt(index))) {
-            index++;
-        }
-        return token.substring(0, index);
+    private static int getFieldId(final Matcher matcher) {
+        return Integer.parseInt(getGroup(matcher, FIELD_ID));
+    }
+
+    private static Datatype getDatatype(final Matcher matcher) {
+        return new Datatype(
+                NodeParser
+                        .getGroup(matcher, DATA_TYPE)
+                        .charAt(0));
+    }
+
+    private static String getValue(final Matcher matcher) {
+        return getGroup(matcher, VALUE);
+    }
+
+    private static String getGroup(final Matcher matcher, final String group) {
+        return Objects.requireNonNull(matcher.group(group));
     }
 }
