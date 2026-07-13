d327d8850ba3b1f4eac7cad0222ce14ccea0d088
frankknoll <Knoll_Frank@web.de>
2026-07-13 23:33:25 +0200

    refactoring

--- app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/DirectionsUrlTemplateFactory.java
+++ app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/DirectionsUrlTemplateFactory.java
@@ -1,65 +1,52 @@
 package de.KnollFrank.routeoptimizerforgooglemaps;
 
-import android.net.Uri;
-
 import java.net.URL;
+import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
+import java.util.stream.IntStream;
 
-import de.KnollFrank.routeoptimizerforgooglemaps.common.URLs;
 import de.KnollFrank.routeoptimizerforgooglemaps.coordinate.Angle;
 import de.KnollFrank.routeoptimizerforgooglemaps.coordinate.Geodetic;
 import de.KnollFrank.routeoptimizerforgooglemaps.coordinate.Unit;
+import de.KnollFrank.routeoptimizerforgooglemaps.route.Route;
+import de.KnollFrank.routeoptimizerforgooglemaps.route.RouteToUrlConverter;
+import de.KnollFrank.routeoptimizerforgooglemaps.route.Stop;
 
 public class DirectionsUrlTemplateFactory {
 
-    // FK-TODO: rufe intern den RouteToUrlConverter nachdem eine neue Methode geschrieben wurde, die eine Route mit totalStops erzeugt analog zum aktuellen Code in createDirectionsUrlTemplate()
     public static URL createDirectionsUrlTemplate(final Geodetic base, int totalStops) {
-        if (totalStops < 1) {
+        return RouteToUrlConverter.getUrl(
+                createRoute(
+                        base,
+                        Geodetic.fromLatitudeLongitude(
+                                new Angle(0.0005, Unit.DEGREES),
+                                new Angle(0.0003, Unit.DEGREES)),
+                        totalStops));
+    }
+
+    private static Route createRoute(final Geodetic base, final Geodetic shift, final int totalStops) {
+        if (totalStops < 2) {
             throw new IllegalArgumentException("totalStops: " + totalStops);
         }
+        final List<Stop> stops = createStops(base, shift, totalStops);
+        return new Route(
+                stops.get(0),
+                stops.subList(1, stops.size() - 1),
+                stops.get(stops.size() - 1));
+    }
 
-        final StringBuilder pathBuilder = new StringBuilder("https://www.google.com/maps/dir");
-        final StringBuilder dataBuilder = new StringBuilder();
-
-        int tokenCountInner = 4 * totalStops;
-        int tokenCountOuter = tokenCountInner + 1;
-
-        // 1. Google Protobuf-Header berechnen
-        dataBuilder
-                .append("!3m2!1e3!4b1")
-                .append("!4m").append(tokenCountOuter)
-                .append("!4m").append(tokenCountInner);
-
-        // 2. Ersten Punkt anlegen (Die echte Apotheke)
-        pathBuilder.append("/").append(Uri.encode("Wegpunkt 1"));
-        dataBuilder.append(
-                String.format(
-                        Locale.US,
-                        "!1m3!2m2!1d%.4f!2d%.4f",
-                        base.getLongitude().toDegrees(),
-                        base.getLatitude().toDegrees()));
-
-        // 3. Die restlichen Dummy-Slots per Micro-Shifting generieren
-        final Geodetic shift =
-                Geodetic.fromLatitudeLongitude(
-                        new Angle(0.0005, Unit.DEGREES),
-                        new Angle(0.0003, Unit.DEGREES));
-        for (int i = 1; i < totalStops; i++) {
-            // Text für das UI-Feld erzeugen
-            final String label = String.format(Locale.US, "Wegpunkt %d", i + 1);
-            pathBuilder.append("/").append(Uri.encode(label));
-
-            // Koordinaten leicht verschieben (ca. 50 Meter pro Schritt)
-            final Geodetic shifted = base.add(shift.mul(i));
-            dataBuilder.append(String.format(Locale.US, "!1m3!2m2!1d%.4f!2d%.4f", shifted.getLongitude().toDegrees(), shifted.getLatitude().toDegrees()));
-        }
-
-        // 4. Alles zusammenfügen
-        pathBuilder
-                .append("/data=")
-                .append(dataBuilder)
-                .append("?entry=ttu");
-
-        return URLs.createUrl(pathBuilder.toString());
+    private static List<Stop> createStops(final Geodetic base, final Geodetic shift, final int totalStops) {
+        return IntStream
+                .range(0, totalStops)
+                .mapToObj(
+                        i ->
+                                new Stop(
+                                        "" + (i + 1),
+                                        String.format(Locale.US, "Wegpunkt %d", i + 1),
+                                        Optional.empty(),
+                                        base.add(shift.mul(i)),
+                                        Optional.empty()))
+                .toList();
     }
 }
