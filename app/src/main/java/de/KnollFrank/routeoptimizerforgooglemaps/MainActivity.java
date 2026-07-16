f5fc3daa7824b08f7412e8e2a76074aa36888173
frankknoll <Knoll_Frank@web.de>
2026-07-16 09:24:49 +0200

    adding "Buy me a coffee"

--- app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/MainActivity.java
+++ app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/MainActivity.java
@@ -5,25 +5,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.slider.Slider;
 
 import java.net.URL;
 
+import de.KnollFrank.routeoptimizerforgooglemaps.billing.BillingHelper;
 import de.KnollFrank.routeoptimizerforgooglemaps.route.RouteTemplateFactory;
 import de.KnollFrank.routeoptimizerforgooglemaps.route.RouteToUrlConverter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BillingHelper.BillingListener {
+
+    private BillingHelper billingHelper;
 
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        billingHelper = new BillingHelper(this, this);
         configurePlanRoute();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (billingHelper != null) {
+            billingHelper.endConnection();
+        }
     }
 
     private void configurePlanRoute() {
@@ -43,6 +57,34 @@ public class MainActivity extends AppCompatActivity {
         this
                 .<Button>findViewById(R.id.btnGenerateTemplate)
                 .setOnClickListener(onBtnGenerateTemplateClick(sliderTotalStops, this));
+
+        // Setup "Buy me a coffee" button if it exists in layout
+        final View btnCoffee = findViewById(R.id.btnCoffee);
+        if (btnCoffee != null) {
+            btnCoffee.setOnClickListener(view -> billingHelper.launchBillingFlow(this, BillingHelper.COFFEE_CAPPUCCINO));
+        }
+    }
+
+    @Override
+    public void onDonationSuccessful() {
+        runOnUiThread(this::showThankYouDialog);
+    }
+
+    @Override
+    public void onBillingError(String message) {
+        runOnUiThread(
+                () ->
+                        Toast
+                                .makeText(this, message, Toast.LENGTH_SHORT)
+                                .show());
+    }
+
+    private void showThankYouDialog() {
+        new MaterialAlertDialogBuilder(this)
+                .setTitle(R.string.thanks_title)
+                .setMessage(R.string.thanks_message)
+                .setPositiveButton(R.string.close, (dialog, which) -> dialog.dismiss())
+                .show();
     }
 
     private static View.OnClickListener onBtnGenerateTemplateClick(final Slider sliderTotalStops,
