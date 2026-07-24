52e6086ce0999cbf6abf90846a7dda2e1834264c
frankknoll <Knoll_Frank@web.de>
2026-07-24 15:50:04 +0200

    Performance Optimization using TransportCostsMatrix

--- app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/optimize/HaversineVehicleRoutingTransportCostsProvider.java
+++ app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/optimize/HaversineVehicleRoutingTransportCostsProvider.java
@@ -1,13 +1,25 @@
 package de.KnollFrank.routeoptimizerforgooglemaps.optimize;
 
 import com.graphhopper.jsprit.core.problem.cost.VehicleRoutingTransportCosts;
+import com.graphhopper.jsprit.core.util.VehicleRoutingTransportCostsMatrix;
 
 import de.KnollFrank.routeoptimizerforgooglemaps.route.Route;
+import de.KnollFrank.routeoptimizerforgooglemaps.route.Stop;
 
 public class HaversineVehicleRoutingTransportCostsProvider implements VehicleRoutingTransportCostsProvider {
 
     @Override
     public VehicleRoutingTransportCosts getVehicleRoutingTransportCosts(final Route route) {
-        return new HaversineTransportCosts();
+        final VehicleRoutingTransportCostsMatrix.Builder matrixBuilder =
+                VehicleRoutingTransportCostsMatrix.Builder.newInstance(true);
+        for (final Stop from : route.stops()) {
+            for (final Stop to : route.stops()) {
+                matrixBuilder.addTransportDistance(
+                        from.id(),
+                        to.id(),
+                        HaversineDistanceCalculator.calculateDistance(from, to));
+            }
+        }
+        return matrixBuilder.build();
     }
 }
--- app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/optimize/OsrmVehicleRoutingTransportCostsProvider.java
+++ app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/optimize/OsrmVehicleRoutingTransportCostsProvider.java
@@ -1,12 +1,14 @@
 package de.KnollFrank.routeoptimizerforgooglemaps.optimize;
 
 import com.graphhopper.jsprit.core.problem.cost.VehicleRoutingTransportCosts;
+import com.graphhopper.jsprit.core.util.VehicleRoutingTransportCostsMatrix;
 
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import de.KnollFrank.routeoptimizerforgooglemaps.route.Route;
+import de.KnollFrank.routeoptimizerforgooglemaps.route.Stop;
 
 public class OsrmVehicleRoutingTransportCostsProvider implements VehicleRoutingTransportCostsProvider {
 
@@ -18,7 +20,27 @@ public class OsrmVehicleRoutingTransportCostsProvider implements VehicleRoutingT
 
     @Override
     public VehicleRoutingTransportCosts getVehicleRoutingTransportCosts(final Route route) throws Exception {
-        return new OsrmTransportCosts(getRoutingMatrix(route));
+        final RoutingMatrix routingMatrix = getRoutingMatrix(route);
+        final VehicleRoutingTransportCostsMatrix.Builder matrixBuilder =
+                VehicleRoutingTransportCostsMatrix.Builder.newInstance(false);
+        for (final Stop from : route.stops()) {
+            for (final Stop to : route.stops()) {
+                final DistanceDuration distanceDuration =
+                        routingMatrix
+                                .getDistanceDurationByStopIdTable()
+                                .get(from.id(), to.id());
+                if (distanceDuration != null) {
+                    matrixBuilder.addTransportDistance(from.id(), to.id(), distanceDuration.distance());
+                    matrixBuilder.addTransportTime(from.id(), to.id(), distanceDuration.duration());
+                } else {
+                    matrixBuilder.addTransportDistance(
+                            from.id(),
+                            to.id(),
+                            HaversineDistanceCalculator.calculateDistance(from, to));
+                }
+            }
+        }
+        return matrixBuilder.build();
     }
 
     private RoutingMatrix getRoutingMatrix(final Route route) throws Exception {
