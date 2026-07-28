5e3dc212156ebb887e403385593cf20b4a2ff6a6
frankknoll <Knoll_Frank@web.de>
2026-07-28 11:04:21 +0200

    Modern Service Indicator

--- app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/feature/ActiveServiceHighlightFeature.java
+++ app/src/main/java/de/KnollFrank/routeoptimizerforgooglemaps/feature/ActiveServiceHighlightFeature.java
@@ -52,7 +52,8 @@ public class ActiveServiceHighlightFeature implements AccessibilityFeature {
                 WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
                         WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE |
-                        WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
+                        WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN |
+                        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 PixelFormat.TRANSLUCENT);
 
         windowManager.addView(highlightView, params);
