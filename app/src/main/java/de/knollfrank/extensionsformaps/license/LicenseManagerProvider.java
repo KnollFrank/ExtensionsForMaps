f0bbd966f9fd1e4b821d59b2f62a731efa3357f6
Frank Knoll <Knoll_Frank@web.de>
2026-08-14 18:26:40 +0200

    refactoring

--- app/src/main/java/de/knollfrank/extensionsformaps/license/LicenseManagerProvider.java
+++ app/src/main/java/de/knollfrank/extensionsformaps/license/LicenseManagerProvider.java
@@ -2,18 +2,23 @@ package de.knollfrank.extensionsformaps.license;
 
 import android.content.Context;
 
+import java.util.Optional;
+
 public class LicenseManagerProvider {
 
-    private static LicenseManager instance;
+    private static Optional<LicenseManager> instance = Optional.empty();
 
     public static LicenseManager getInstance(final Context context) {
-        if (instance == null) {
-            instance = GumroadLicenseManagerFactory.createGumroadLicenseManager(context.getApplicationContext());
+        if (instance.isEmpty()) {
+            instance =
+                    Optional.of(
+                            GumroadLicenseManagerFactory.createGumroadLicenseManager(
+                                    context.getApplicationContext()));
         }
-        return instance;
+        return instance.orElseThrow();
     }
 
     public static void setInstance(final LicenseManager licenseManager) {
-        instance = licenseManager;
+        instance = Optional.of(licenseManager);
     }
 }
