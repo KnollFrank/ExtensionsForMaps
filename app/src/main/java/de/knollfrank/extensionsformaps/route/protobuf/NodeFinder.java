3587c5c6ef775debe97f1139e79be08b49a99c94
frankknoll <Knoll_Frank@web.de>
2026-08-15 20:20:53 +0200

    refactoring

--- app/src/main/java/de/knollfrank/extensionsformaps/route/protobuf/NodeFinder.java
+++ app/src/main/java/de/knollfrank/extensionsformaps/route/protobuf/NodeFinder.java
@@ -10,18 +10,25 @@ public class NodeFinder {
         final List<Node> all4mContainers = new ArrayList<>();
         findAllContainersByFieldId(rootNodes, 4, all4mContainers);
         for (final Node container4m : all4mContainers) {
-            final List<Node> waypointChildrenOf4mContainer = new ArrayList<>();
-            for (final Node child : container4m.children()) {
-                if (child.fieldId() == 1 && child.isContainer()) {
-                    waypointChildrenOf4mContainer.add(child);
-                }
-            }
+            final List<Node> waypointChildrenOf4mContainer = getWaypointChildren(container4m);
             // Wir suchen den 4m-Container, der EXAKT so viele 1m-Kinder hat, wie wir Wegpunkte im Pfad gefunden haben
             if (waypointChildrenOf4mContainer.size() == expectedCount) {
                 return waypointChildrenOf4mContainer;
             }
         }
         return List.of();
+    }
+
+    private static List<Node> getWaypointChildren(final Node node) {
+        return node
+                .children()
+                .stream()
+                .filter(NodeFinder::isWaypoint)
+                .toList();
+    }
+
+    private static boolean isWaypoint(final Node node) {
+        return node.fieldId() == 1 && node.isContainer();
     }
 
     // FK-TODO: refactor
