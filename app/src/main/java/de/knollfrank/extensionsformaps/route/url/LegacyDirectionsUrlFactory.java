a845c51a7cb798b3d3a0c3c3fd6f4c64a126f574
frankknoll <Knoll_Frank@web.de>
2026-08-18 13:25:22 +0200

    refactoring

--- app/src/main/java/de/knollfrank/extensionsformaps/route/url/LegacyDirectionsUrlFactory.java
+++ app/src/main/java/de/knollfrank/extensionsformaps/route/url/LegacyDirectionsUrlFactory.java
@@ -3,7 +3,6 @@ package de.knollfrank.extensionsformaps.route.url;
 import java.net.URL;
 import java.util.Optional;
 
-// FK-TODO: move createLegacyDirectionsUrl() into LegacyDirectionsUrl and check in it's constructor. Dito for ModernDirectionsUrlFactory
 class LegacyDirectionsUrlFactory {
 
     public static Optional<LegacyDirectionsUrl> createLegacyDirectionsUrl(final URL url) {
