26a2307ea732caeb3028ee76313559cfb37c27d1
frankknoll <Knoll_Frank@web.de>
2026-08-21 23:47:55 +0200

    refactoring

--- app/src/main/java/de/knollfrank/extensionsformaps/feature/OptimizationTypeDialog.java
+++ app/src/main/java/de/knollfrank/extensionsformaps/feature/OptimizationTypeDialog.java
@@ -3,7 +3,10 @@ package de.knollfrank.extensionsformaps.feature;
 import android.content.Context;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.common.collect.ImmutableBiMap;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 
 import de.knollfrank.extensionsformaps.R;
@@ -19,32 +22,39 @@ public class OptimizationTypeDialog {
         void onCancel();
     }
 
+    private static final ImmutableBiMap<OptimizationType, Integer> optimizationTypeToStringRes =
+            ImmutableBiMap.of(
+                    OptimizationType.FIXED_DESTINATION, R.string.settings_type_fixed_destination,
+                    OptimizationType.ANY_DESTINATION, R.string.settings_type_any_destination);
+
+    private static final List<OptimizationType> optimizationTypes = optimizationTypeToStringRes.keySet().asList();
+
     public static void show(final Context context, final Callback callback) {
-        final AtomicReference<OptimizationType> selectedType = new AtomicReference<>(SortConfig.getOptimizationType(context));
+        final AtomicReference<OptimizationType> selectedOptimizationType = new AtomicReference<>(SortConfig.getOptimizationType(context));
         new MaterialAlertDialogBuilder(context)
                 .setTitle(R.string.sort_dialog_title)
                 .setSingleChoiceItems(
-                        new String[]{
-                                context.getString(R.string.settings_type_fixed_destination),
-                                context.getString(R.string.settings_type_any_destination)
-                        },
-                        selectedType.get() == OptimizationType.FIXED_DESTINATION ? 0 : 1,
-                        (dialog, which) ->
-                                selectedType.set(
-                                        which == 0 ?
-                                                OptimizationType.FIXED_DESTINATION :
-                                                OptimizationType.ANY_DESTINATION))
+                        getSingleChoiceItems(context),
+                        optimizationTypes.indexOf(selectedOptimizationType.get()),
+                        (dialog, which) -> selectedOptimizationType.set(optimizationTypes.get(which)))
                 .setCancelable(false)
                 .setPositiveButton(
                         R.string.ok,
                         (dialog, which) -> {
-                            SortConfig.setOptimizationType(context, selectedType.get());
-                            callback.onOptimizationTypeSelected(selectedType.get());
+                            SortConfig.setOptimizationType(context, selectedOptimizationType.get());
+                            callback.onOptimizationTypeSelected(selectedOptimizationType.get());
                         })
                 .setNegativeButton(
                         R.string.cancel,
                         (dialog, which) -> callback.onCancel())
                 .setOnCancelListener(dialog -> callback.onCancel())
                 .show();
+    }
+
+    private static String[] getSingleChoiceItems(final Context context) {
+        return optimizationTypes
+                .stream()
+                .map(optimizationType -> context.getString(Objects.requireNonNull(optimizationTypeToStringRes.get(optimizationType))))
+                .toArray(String[]::new);
     }
 }
