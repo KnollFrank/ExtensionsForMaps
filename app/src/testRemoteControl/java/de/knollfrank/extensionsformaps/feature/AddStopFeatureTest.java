d80cb243c9076257b9bf163e6a819c202107bdcc
frankknoll <Knoll_Frank@web.de>
2026-08-25 10:41:59 +0200

    refactoring

--- app/src/testRemoteControl/java/de/knollfrank/extensionsformaps/feature/AddStopFeatureTest.java
+++ app/src/testRemoteControl/java/de/knollfrank/extensionsformaps/feature/AddStopFeatureTest.java
@@ -21,34 +21,36 @@ import de.knollfrank.extensionsformaps.accessibility.StopCountParser;
 @RunWith(RobolectricTestRunner.class)
 public class AddStopFeatureTest {
 
-    private AddStopFeature feature;
-    private RouteUrlRequester urlRequester;
-    private RouteUrlRequester.RouteUrlCallback callback;
+    private AddStopFeature addStopFeature;
+    private RouteUrlRequester routeUrlRequester;
+    private RouteUrlRequester.RouteUrlCallback routeUrlCallback;
 
     @Before
     public void setUp() {
-        final AccessibilityService accessibilityService = mock(AccessibilityService.class);
-        urlRequester = mock(RouteUrlRequester.class);
-        callback = mock(RouteUrlRequester.RouteUrlCallback.class);
-        final GoogleMapsContext googleMapsContext =
-                new GoogleMapsContext(
-                        "Add stops",
-                        "stops",
-                        new StopCountParser(Pattern.compile("(\\d+) stops")));
-        feature = new AddStopFeature(accessibilityService, googleMapsContext, urlRequester, callback);
+        routeUrlRequester = mock(RouteUrlRequester.class);
+        routeUrlCallback = mock(RouteUrlRequester.RouteUrlCallback.class);
+        addStopFeature =
+                new AddStopFeature(
+                        mock(AccessibilityService.class),
+                        new GoogleMapsContext(
+                                "Add stops",
+                                "stops",
+                                new StopCountParser(Pattern.compile("(\\d+) stops"))),
+                        routeUrlRequester,
+                        routeUrlCallback);
     }
 
     @Test
     public void testOnGoogleMapsEvent_ClickAddStops_TriggersUrlRequest() {
         // Given
-        feature.onStopCountUpdated(15, new Rect()); // Above limit
+        addStopFeature.onStopCountUpdated(15, new Rect()); // Above limit
         final AccessibilityEvent event = AccessibilityEvent.obtain(AccessibilityEvent.TYPE_VIEW_CLICKED);
         event.getText().add("Add stops");
 
         // When
-        feature.onGoogleMapsEvent(event, null);
+        addStopFeature.onGoogleMapsEvent(event, null);
 
         // Then
-        verify(urlRequester).requestRouteUrl(callback);
+        verify(routeUrlRequester).requestRouteUrl(routeUrlCallback);
     }
 }
