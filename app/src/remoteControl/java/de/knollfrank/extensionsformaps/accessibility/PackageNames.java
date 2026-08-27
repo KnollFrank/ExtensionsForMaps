32e0cef6fe2a94549e6916134afdf966d3f35555
frankknoll <Knoll_Frank@web.de>
2026-08-27 14:30:12 +0200

    refactoring

--- app/src/remoteControl/java/de/knollfrank/extensionsformaps/accessibility/PackageNames.java
+++ app/src/remoteControl/java/de/knollfrank/extensionsformaps/accessibility/PackageNames.java
@@ -2,6 +2,9 @@ package de.knollfrank.extensionsformaps.accessibility;
 
 public class PackageNames {
 
+    private PackageNames() {
+    }
+
     public static final String GOOGLE_MAPS_PACKAGE = "com.google.android.apps.maps";
     public static final String RESOLVER_PACKAGE = "com.android.intentresolver";
     public static final String SYSTEM_PACKAGE = "android";
