bfd5fe7040eb64d7b4b983af19eff7014fe0da08
frankknoll <Knoll_Frank@web.de>
2026-09-01 17:36:04 +0200

    removing a misleading TODO

--- app/src/main/java/de/knollfrank/extensionsformaps/route/DeliveryGroups.java
+++ app/src/main/java/de/knollfrank/extensionsformaps/route/DeliveryGroups.java
@@ -5,7 +5,6 @@ import java.util.stream.IntStream;
 
 public class DeliveryGroups {
 
-    // FK-TODO: replace magic number 10
     public static final List<DeliveryGroup> DELIVERY_GROUPS = createDeliveryGroups(10);
 
     private static List<DeliveryGroup> createDeliveryGroups(final int num) {
