d33cc069a392b5913efd3c3dfa730351328ebba6
frankknoll <Knoll_Frank@web.de>
2026-09-01 16:52:38 +0200

    adding a TODO

--- app/src/main/java/de/knollfrank/extensionsformaps/route/DeliveryGroups.java
+++ app/src/main/java/de/knollfrank/extensionsformaps/route/DeliveryGroups.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 
 public class DeliveryGroups {
 
+    // FK-TODO: replace magic number 10
     public static final List<DeliveryGroup> DELIVERY_GROUPS = createDeliveryGroups(10);
 
     private static List<DeliveryGroup> createDeliveryGroups(final int num) {
